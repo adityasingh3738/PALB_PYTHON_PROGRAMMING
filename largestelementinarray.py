61f1970941672c20bc8580676249b5246b6346a6
Aditya Singh <adityasingh290306@gmail.com>
2026-02-18 09:34:15 +0530

    Create largestelementinarray.py

---
+++ largestelementinarray.py
@@ -0,0 +1,19 @@
+#Method1
+class Solution:
+    def largest(self, arr):
+        arr.sort(reverse=True)
+        return arr[0]
+
+#Method2
+class Solution:
+    def largest(self, arr):
+        return max(arr)
+
+#Method3
+class Solution:
+    def largest(self, arr):
+        max=arr[0]
+        for i in arr:
+            if i>max:
+                max=i
+        return max
