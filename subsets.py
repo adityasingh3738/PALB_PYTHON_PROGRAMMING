b77065392471bcdeed6a6c0dfc781753af622dee
Aditya Singh <adityasingh290306@gmail.com>
2026-04-25 07:41:32 +0530

    Create subsets.py

---
+++ subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums):
+        result = []
+
+        def backtrack(start, path):
+            result.append(path[:])   # add current subset
+
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return result
